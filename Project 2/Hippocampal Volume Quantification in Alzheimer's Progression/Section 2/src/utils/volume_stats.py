ece0feb66dc2271ce94a01cb477cd26e11b3794b
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2021-09-15 17:01:45 +0300

    Project tree cleanup and review correction

--- Project 2/Hippocampal Volume Quantification in Alzheimer's Progression/Section 2/src/utils/volume_stats.py
+++ Project 2/Hippocampal Volume Quantification in Alzheimer's Progression/Section 2/src/utils/volume_stats.py
@@ -3,6 +3,7 @@ Contains various functions for computing statistics over 3D volumes
 """
 import numpy as np
 
+
 def Dice3d(a, b):
     """
     This will compute the Dice Similarity coefficient for two 3-dimensional volumes
@@ -17,16 +18,18 @@ def Dice3d(a, b):
         float
     """
     if len(a.shape) != 3 or len(b.shape) != 3:
-        raise Exception(f"Expecting 3 dimensional inputs, got {a.shape} and {b.shape}")
+        raise Exception(
+            f"Expecting 3 dimensional inputs, got {a.shape} and {b.shape}")
 
     if a.shape != b.shape:
-        raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
+        raise Exception(
+            f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
 
     # TASK: Write implementation of Dice3D. If you completed exercises in the lessons
     # you should already have it.
     # <YOUR CODE HERE>
-    intersection = np.sum(a*b)
-    volumes = np.sum(a) + np.sum(b)
+    intersection = np.sum((a > 0) * (b > 0))
+    volumes = np.sum(a > 0) + np.sum(b > 0)
 
     if volumes == 0:
         return -1
